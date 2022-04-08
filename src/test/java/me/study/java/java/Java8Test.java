ec79e54a9d75c26297c4d0d1e5f06bb8ba168cdb
Jeongsu Yang <mycat83@gmail.com>
2022-04-08 22:22:11 +0900

    refactor: Stream API 테스트

--- src/test/java/me/study/java/java/Java8Test.java
+++ src/test/java/me/study/java/java/Java8Test.java
@@ -4,12 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Java 8 기능")
 public class Java8Test {
@@ -74,5 +82,50 @@ public class Java8Test {
         long afterTime = System.currentTimeMillis();
         long diffTime = afterTime - beforeTime;
         System.out.println("실행 시간: " + diffTime + "ms");
+    }
+
+    @Test
+    @DisplayName("Stream API 테스트")
+    void stream() {
+        int sum1 = Stream.iterate(1, n -> n + 1)
+                .limit(5)
+                .skip(2)
+                .peek(System.out::println)
+                .mapToInt(it -> it)
+                .sum();
+
+        assertThat(sum1).isEqualTo(12);
+
+        Integer sum2 = Stream.of(1, 2, 3)
+                .reduce(0, Integer::sum);
+
+        assertThat(sum2).isEqualTo(6);
+
+        int max = new Random().ints(5)
+                .filter(it -> it > 0)
+                .peek(System.out::println)
+                .max()
+                .orElse(0);
+
+        assertThat(max).isGreaterThan(0);
+
+        List<String> collect = Pattern.compile(",")
+                .splitAsStream("a,a,b,c,d")
+                .distinct()
+                .peek(System.out::println)
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        assertThat(collect).containsExactly("A", "B", "C", "D");
+
+        List<List<Integer>> lists = List.of(List.of(5, 1, 3), List.of(4, 2));
+        String joining = lists.stream()
+                .flatMap(Collection::stream)
+                .sorted()
+                .peek(System.out::println)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ", "<", ">"));
+
+        assertThat(joining).isEqualTo("<1, 2, 3, 4, 5>");
     }
 }
